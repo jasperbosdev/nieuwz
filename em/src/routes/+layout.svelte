34c5b6aa6436e6a1a56e17d064a587dcb924695c
Lionz <96448965+lionzv2@users.noreply.github.com>
2024-06-12 23:04:21 +0200

    create layout as base

---
+++ em/src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import "../style.css";
+  import { onMount } from 'svelte';
+  import { createBackground } from '../utils/background';
+
+  let currentYear = new Date().getFullYear();
+
+  onMount(() => {
+    const container = document.querySelector('.viewport') as HTMLElement;
+    createBackground(container);
+  });
+</script>
+
+<div class="viewport"></div> <!-- background -->
+
+<div class="container mx-auto">
+  <div class="card"> <!-- container, main card and card content -->
+    <div class="card-content">
+
+      <div class="flex justify-between nav items-center"> <!-- navbar -->
+        <div class="brand avi">
+          <img src="https://a.ppy.sh/6519705?1598577889.jpeg" alt="logo">
+        </div>
+        <div class="brand avi flex gap-x-14">
+          <div href="#/"><a href="#/">/home</a></div>
+          <div href="#/"><a href="#/">/content</a></div>
+          <div href="#/"><a href="#/">/contact</a></div>
+        </div>
+        <div class="brand avi">
+          <a href="#/"><i class="fa fa-heart fa-xl transition duration-200 hover:-translate-y-1.5"></i></a>
+        </div>
+      </div>
+
+
+      <div class="py-20">
+        <slot />
+      </div>
+
+
+      <footer class="text-center font-normal border"> <!-- footer (duhhh) -->
+        <p>© {currentYear} 🐈 em</p>
+      </footer>
+
+    </div>
+  </div>
+</div>
