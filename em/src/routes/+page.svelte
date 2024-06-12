94556b81feb9bc17b48eef9c8f8807af80c32d5b
Lionz <96448965+lionzv2@users.noreply.github.com>
2024-06-12 23:04:36 +0200

    starting on index/home

--- em/src/routes/+page.svelte
+++ em/src/routes/+page.svelte
@@ -1,31 +1,14 @@
 <script lang="ts">
-  import "../style.css";
-  import { onMount } from 'svelte';
-  import { createBackground } from '../utils/background';
-
-  onMount(() => {
-    const container = document.querySelector('.viewport') as HTMLElement;
-    createBackground(container);
-  });
+    import { onMount } from 'svelte';
+  
+    onMount(() => {});
 </script>
-
-<div class="viewport"></div>
-<div class="card">
-  <div class="card-content">
-    <div class="flex justify-between nav items-center">
-      <div class="brand avi px-2">
-        <img src="https://cdn.discordapp.com/avatars/673304389875925003/313942663304fbff713fec390cc6b657.png" alt="logo">
-      </div>
-      <div class="brand avi flex gap-x-14">
-        <div href="#/">/home</div>
-        <div href="#/">/content</div>
-        <div href="#/">/contact</div>
-      </div>
-      <div class="brand avi">
-        <i class="fa fa-heart fa-xl"></i>
-      </div>
+  
+<div class="grid grid-rows-1 grid-flow-col">
+    <div class="me-card">
+        em 🐈
     </div>
-  </div>
+    <div class="activity-card">
+        blablabla
+    </div>
 </div>
-
-<slot />
