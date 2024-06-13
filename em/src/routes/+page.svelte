7bfcf84df7cfa84a185f9a7b78719a084218fffe
Lionz <96448965+lionzv2@users.noreply.github.com>
2024-06-13 22:42:06 +0200

    small changes, added divider

--- em/src/routes/+page.svelte
+++ em/src/routes/+page.svelte
@@ -5,16 +5,16 @@
     onMount(() => {});
 </script>
 
-<div class="grid grid-rows-1 grid-flow-col"> <!-- maybe add 1.5em x padding to even with nav items-->
+<div class="grid grid-rows-1 grid-flow-col mb-12"> <!-- maybe add 1.5em x padding to even with nav items-->
     <div class="me-card">
         <p class="bold text-6xl"><span class="text-fuchsia-400">Ir</span>cle</p>
         <div class="flex items-center socials mt-4">
             <div class="deck-border py-2 px-4 flex gap-x-4"> <!-- maybe remove border class n padding -->
-                <a href="#/" class="transition duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-envelope"></i></a>
-                <a href="#/" class="transition duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
-                <a href="#/" class="transition duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
-                <a href="#/" class="transition duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
-                <a href="#/" class="transition duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
+                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-envelope"></i></a>
+                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
+                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
+                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
+                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
             </div>
         </div>
     </div>
@@ -23,4 +23,14 @@
             <Activity /> <!-- using lanyard for discord activity; spotify, games etc -->
         </div>
     </div>
+</div>
+
+<!-- divider -->
+<!-- <div class="mb-12">
+    <div class="split deck-split text-center">🐈</div> 
+</div> -->
+
+<div class="yapping">
+    wdwqiodqowidj oqwdoqiwoqwjdqow qwoi qwoi iojdqwiodqwoiqwd oijqwd iojdqw oijwqd iojwqd qwd ijoqwd qwd wqd iojwd oiqwj oqiwdqowi qwoi qwoi 
+    qwdiqwjd iqwojqwiqwo qwoidqwo qwoiqwoidqw dqwm qwdm qw mw mmm, wdqwdqwok  qwodkqo!
 </div>
