592b1d2e71bad9a8d3a3d6d6741eccb50d1cc540
lionzv2 <stefand1916@gmail.com>
2024-06-14 13:36:55 +0200

    add spotify song title and album cover function

--- em/src/utils/activity.svelte
+++ em/src/utils/activity.svelte
@@ -2,7 +2,7 @@
     import { useLanyard } from 'sk-lanyard';
     import { onMount } from 'svelte';
 
-    const presence = useLanyard({ method: 'ws', id: '673304389875925003' });
+    const presence = useLanyard({ method: 'ws', id: '726110048287326288' });
 
     let localTime = '';
 
@@ -24,24 +24,37 @@
     });
 </script>
 
-<div class="activity-info text-end">
-    {#if $presence}
-        @em 🐈<br>
-        <!-- activity<br> --> <!-- todo -->
-        {$presence.discord_status || "error fetching status"}<br>
-        <!-- discord status msg -->
-        {#if $presence.activities && $presence.activities[0] && $presence.activities[0].state}
-            💬: {$presence.activities[0].state}<br>
+<div class="grid grid-cols-[1fr_auto] gap-8 deck-border items-center">
+    <div class="activity-info text-end pl-5">
+            @em 🐈<br>
+            <!-- activity<br> --> <!-- todo -->
+            {#if $presence}
+                {$presence.discord_status || "error fetching status"}<br>
+                {:else if $presence === null}
+                    Error fetching data.<br>
+                {:else}
+                    loading...<br>
+            {/if}
+            {#if $presence}
+                {#if $presence.spotify}
+                    🎶 {($presence.spotify.song.length > 20) ? `${$presence.spotify.song.slice(0, 20)}...` : $presence.spotify.song}<br>
+                    <!-- by {$presence.spotify.artist}<br> -->
+                {/if}
+            {/if}
+            {localTime}
+    </div>
+    <div class="activity-preview">
+        <!-- <img src="https://via.placeholder.com/128" alt="activity-img" class="rounded-lg activity-img"> -->
+        <!-- <img src="https://a.ppy.sh/6519705?1598577889.jpeg" alt="activity-img" class="rounded-lg activity-img"> -->
+        <!-- probably write a function to take and display the discord user's pfp -->
+        {#if $presence}
+            {#if $presence.spotify}
+                <img src="{$presence.spotify.album_art_url}" alt="spotify-album" class="rounded-lg activity-img">
+            {:else}
+                <img src="https://via.placeholder.com/128" alt="placeholder-img" class="rounded-lg activity-img">
+            {/if}
         {/if}
-        {localTime}
-    {:else if $presence === null}
-        Error fetching data.
-    {:else}
-        loading...
-    {/if}
+    </div>
 </div>
-<div class="activity-preview">
-    <!-- <img src="https://via.placeholder.com/128" alt="activity-img" class="rounded-lg activity-img"> -->
-    <img src="https://a.ppy.sh/6519705?1598577889.jpeg" alt="activity-img" class="rounded-lg activity-img">
-    <!-- probably write a function to take and display the discord user's pfp -->
-</div>
+
+<!-- {JSON.stringify($presence, null, 2)} -->
