282464f73df4bc01e355907adab2eee1896909de
Lionz <96448965+lionzv2@users.noreply.github.com>
2024-07-07 20:06:45 +0200

    add rgb text for Sqwore artist

---
+++ em/src/utils/rainbowtext.ts
@@ -0,0 +1,26 @@
+export function hslToRgba(h: number, s: number, l: number, a: number): string {
+    s /= 100;
+    l /= 100;
+    const k = (n: number) => (n + h / 30) % 12;
+    const a2 = s * Math.min(l, 1 - l);
+    const f = (n: number) =>
+      l - a2 * Math.max(Math.min(k(n) - 3, 9 - k(n), 1), -1);
+    return `rgba(${f(0) * 255}, ${f(8) * 255}, ${f(4) * 255}, ${a})`;
+}
+
+let hue = 0;
+const speed = 4; // Adjust the speed of the color change
+
+export function applyRainbowEffect() {
+  const elements = document.querySelectorAll('.rgbtq');
+  elements.forEach(element => {
+    const rgbaColor = hslToRgba(hue, 100, 50, 1);
+    (element as HTMLElement).style.color = rgbaColor;
+  });
+  hue = (hue + speed) % 360;
+}
+
+export function startRainbowEffect(interval: number = 50) {
+  const intervalId = setInterval(applyRainbowEffect, interval); // Adjust the interval for smoother animation
+  return () => clearInterval(intervalId);
+}
