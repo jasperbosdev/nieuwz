eb6b86cd39ca54c980a272a732751836ac81159d
Lionz <96448965+lionzv2@users.noreply.github.com>
2024-08-09 02:04:53 +0200

    /home add typed.js strings and add social links and made responsive

--- em/src/routes/+page.svelte
+++ em/src/routes/+page.svelte
@@ -1,25 +1,43 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import Typed from 'typed.js';
     import tilt  from 'svelte-tilt';
-    import Activity from '../utils/activity.svelte'; // Ensure the path is correct
+    import Activity from '../utils/activity.svelte';
+    import Projects from '../utils/projects.svelte';
 
-    onMount(() => {});
+    onMount(() => {
+      const options = {
+         strings: ["Monster Ultra Addict", "You should play on komako.pw!", "Frikandelbroodje Enjoyer", 
+                   "Noob frontend dev", "Fumo Connaisseur", "awrff >w&lt;"],
+         typeSpeed: 40,
+         startDelay: 250,
+         backSpeed: 30,
+         backDelay: 3300,
+         loop: true,
+         showCursor: true
+      };
+
+      new Typed('.typed', options);
+   });
 </script>
 
-<div class="grid grid-rows-1 grid-flow-col mb-20"> <!-- maybe add 1.5em x padding to even with nav items-->
-    <div class="me-card">
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10 gap-10 md:gap-0"> <!-- maybe add 1.5em x padding to even with nav items-->
+    <div class="me-card flex flex-col justify-center md:justify-start items-center md:items-start">
         <p class="bold text-6xl"><span class="text-fuchsia-400">Li</span>onz</p>
+        <div class="">
+            <p class="typed flex flex-row font-sm"></p>
+        </div>
         <div class="flex items-center socials mt-4">
-            <div class="deck-border py-2 px-4 flex gap-x-4"> <!-- maybe remove border class n padding -->
+            <div class="deck-border py-2 px-4 flex gap-x-4" use:tilt={{ strength: 0.8, scale: 1.0, xAxis: false  }}> <!-- maybe remove border class n padding -->
                 <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-envelope"></i></a>
-                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
-                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
-                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
-                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
+                <a href="https://t.me/dumpup" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
+                <a href="https://github.com/leeuwz" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
+                <a href="https://youtube.com/@stupidpup" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
+                <a href="https://x.com/leeuwuz" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
             </div>
         </div>
     </div>
-    <div class="activity-card flex justify-end items-center">
+    <div class="activity-card flex justify-center md:justify-end items-center">
         <div class="grid grid-cols-[1fr_auto] deck-border items-center" use:tilt={{ strength: 1.5, scale: 1.1 }}>
             <Activity /> <!-- using lanyard for discord activity; spotify, games etc -->
         </div>
@@ -31,9 +49,11 @@
     <div class="split deck-split text-center">🐈</div> 
 </div> -->
 
-<div class="yapping">
+<!-- <div class="yapping">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tempus est. 
     Nullam dignissim sem at mattis vehicula. Suspendisse accumsan ut purus sollicitudin dignissim. 
     Proin mollis lectus vitae elit tincidunt molestie. Nunc congue luctus tortor, id viverra risus maximus sit amet. 
     Morbi egestas augue ante, vel commodo quam vehicula ut. Donec pellentesque nunc tristique elit bibendum, ac sagittis mi rhoncus.
-</div>
+</div> -->
+
+<Projects />
