e88024c90c8d74c02d88705658b9621ff917793b
Lionz <Jasperbos1234@gmail.com>
2024-09-04 16:52:46 +0200

    update socials and remove unused content /home

--- em/src/routes/+page.svelte
+++ em/src/routes/+page.svelte
@@ -19,13 +19,15 @@
 
       new Typed('.typed', options);
    });
+
+   const mail = "dxmpup@proton.me"
 </script>
 
 <svelte:head>
    <title>@lionz 🐈</title>
 </svelte:head>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10 gap-10 md:gap-0"> <!-- maybe add 1.5em x padding to even with nav items-->
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10 gap-10 md:gap-0">
     <div class="me-card flex flex-col justify-center md:justify-start items-center md:items-start">
         <p class="bold text-6xl"><span class="text-fuchsia-400">Li</span>onz</p>
         <div class="">
@@ -33,11 +35,11 @@
         </div>
         <div class="flex items-center socials mt-4">
             <div class="deck-border py-2 px-4 flex gap-x-4" use:tilt={{ strength: 0.8, scale: 1.0, xAxis: false  }}> <!-- maybe remove border class n padding -->
-                <a href="#/" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-envelope"></i></a>
-                <a href="https://t.me/dumpup" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
-                <a href="https://github.com/leeuwz" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
-                <a href="https://youtube.com/@stupidpup" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
-                <a href="https://x.com/leeuwuz" target="_blank" class="transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
+                <a href="mailto:{mail}" target="_blank" class="social transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-envelope"></i></a>
+                <a href="https://t.me/dumpup" target="_blank" class="social transition delay-0 duration-200 hover:-translate-y-1.5"><i class="far fa-lg fa-comments"></i></a>
+                <a href="https://github.com/leeuwz" target="_blank" class="social transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-github"></i></a>
+                <a href="https://youtube.com/@stupidpup" target="_blank" class="social transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-youtube"></i></a>
+                <a href="https://x.com/leeuwuz" target="_blank" class="social transition delay-0 duration-200 hover:-translate-y-1.5"><i class="fab fa-lg fa-x-twitter"></i></a>
             </div>
         </div>
     </div>
@@ -51,13 +53,6 @@
 <!-- divider -->
 <!-- <div class="mb-20">
     <div class="split deck-split text-center">🐈</div> 
-</div> -->
-
-<!-- <div class="yapping">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a tempus est. 
-    Nullam dignissim sem at mattis vehicula. Suspendisse accumsan ut purus sollicitudin dignissim. 
-    Proin mollis lectus vitae elit tincidunt molestie. Nunc congue luctus tortor, id viverra risus maximus sit amet. 
-    Morbi egestas augue ante, vel commodo quam vehicula ut. Donec pellentesque nunc tristique elit bibendum, ac sagittis mi rhoncus.
 </div> -->
 
 <Projects />
